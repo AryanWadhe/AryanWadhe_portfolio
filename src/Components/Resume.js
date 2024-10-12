e4fefb70b1fd3bf525e96fe0ac9a35a2ec280399
Aryan Wadhe <lcs2021049@iiitl.ac.in>
2024-10-12 17:58:45 +0530

    Resume Updated

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -8,29 +8,28 @@ pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/b
 
 
 const Resume = () => {
-  const[wid, setwid]=useState(window.innerWidth);
+  const [wid, setwid] = useState(window.innerWidth);
 
-  const handleResize=()=>{
+  const handleResize = () => {
     setwid(window.innerWidth);
   }
 
   window.addEventListener("load", handleResize);
   window.addEventListener("resize", handleResize);
-
+  
   return (
     <div className='ResumePage'>
       <Document file={pdf} className="resumeview">
-          <Page pageNumber= {1} scale={wid<700 ? ( wid>475? 0.7: 0.5): 1}/>
+        <Page pageNumber={1} scale={wid < 700 ? (wid > 475 ? 0.7 : 0.5) : 1} />
       </Document>
 
-      <a href={pdf} target='_blank' download="Aryan's Resume">
+      <a href={pdf} target='_blank' rel="noopener noreferrer" download="Aryan's Resume">
         <button className='downloadCV' type='button'>
-          <h3><BsDownload/>&nbsp; Download CV</h3>
+          <h3><BsDownload />&nbsp; Download CV</h3>
         </button>
       </a>
-
     </div>
-  )
+  );
 }
 
-export default Resume
+export default Resume;
